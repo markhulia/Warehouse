7457db64dfac355779bc7fd450bdf32ad068ee2f
markhulia <markhulia@gmail.com>
2015-05-21 09:39:22 +0200

    commit

--- mobile/src/main/java/com/tinyvoice/warehouse/MainActivity.java
+++ mobile/src/main/java/com/tinyvoice/warehouse/MainActivity.java
@@ -35,6 +35,7 @@ public class MainActivity extends ActionBarActivity {
     public static final int NOTIFICATION_ID = 1;
     public static final String ItemID = "Item Id";
     public static final String ItemName = "Item Name";
+    private static final String EXTRA_VOICE_REPLY = "extra_voice_reply";
 //    public static final String numberOfPackages = null;
 
     @Override
@@ -43,209 +44,67 @@ public class MainActivity extends ActionBarActivity {
         setContentView(R.layout.activity_main);
     }
 
-    private PendingIntent getActivityPendingIntent() {
-
-        Intent activityIntent = new Intent(this, MainActivity.class);
-        activityIntent.addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
-        return PendingIntent.getActivity(this, 0, activityIntent,
-                PendingIntent.FLAG_UPDATE_CURRENT);
-    }
-
-
-//    public void onBigTextStyleNotificationButtonClick(View view) {
-//
-//        String longText = "Without BigTextStyle, only a single line of text would be visible. " +
-//                "Any additional text would not appear directly on the notification. " +
-//                "The entire first line would not even be on the notification if it were too long! " +
-//                "Text that doesn't fit in a standard notification becomes ellipsized. " +
-//                "That is, the characters that don't fit are removed and replaced by ellipsis.";
-//
-//        PendingIntent activityPendingIntent = getActivityPendingIntent();
-//        Toast.makeText(this, "BIG Text Style notification", Toast.LENGTH_SHORT).show();
-////        NotificationCompat.BigTextStyle bigTextStyle = new NotificationCompat.BigTextStyle()
-////                .bigText(longText);
-//
-//
-//        Notification bigTextStyleNotification = new Notification.Builder(this)
-//                .setContentTitle("Aaaw yiis Big Text")
-//                .setContentText(longText)
-//                .setSmallIcon(R.drawable.ic_action_good)
-//                .setContentIntent(activityPendingIntent)
-//                .setStyle(new Notification.BigTextStyle()
-//                        .bigText(longText))
-//                .build();
-//
-//
-//        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
-//        notificationManager.notify(NOTIFICATION_ID, bigTextStyleNotification);
-//
-//    }
-
-    private PendingIntent getConversationPendingIntent(String chattingWith, int requestCode) {
-        Intent conversationIntent = new Intent(this, ChatDetailActivity.class);
-
-        if (chattingWith != null) {
-            conversationIntent.putExtra(ChatDetailActivity.EXTRA_CHATTING_WITH, chattingWith);
-        }
-
-        TaskStackBuilder taskStackBuilder = TaskStackBuilder.create(this);
-        taskStackBuilder.addParentStack(ChatDetailActivity.class);
-        taskStackBuilder.addNextIntent(conversationIntent);
-
-        return taskStackBuilder.getPendingIntent(requestCode, PendingIntent.FLAG_CANCEL_CURRENT);
-    }
-
-    private PendingIntent getActionFeedbackPendingIntent(String actionFeedback, int requestCode) {
-
-        Intent actionFeedbackIntent = new Intent(this, ActionFeedbackActivity.class);
-        actionFeedbackIntent.putExtra(ActionFeedbackActivity.EXTRA_ACTION_FEEDBACK, actionFeedback);
-
-        TaskStackBuilder taskStackBuilder = TaskStackBuilder.create(this)
-                .addParentStack(ActionFeedbackActivity.class)
-                .addNextIntent(actionFeedbackIntent);
-
-        return taskStackBuilder.getPendingIntent(requestCode, PendingIntent.FLAG_CANCEL_CURRENT);
-    }
-
-    private PendingIntent getMainActivityPendingIntent() {
-        Intent mainActivityIntent = new Intent(this, MainActivity.class);
-        mainActivityIntent.addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
-        return PendingIntent.getActivity(this, 0,
-                mainActivityIntent, PendingIntent.FLAG_CANCEL_CURRENT);
-    }
-
-    //Perform actions on notifications from wearable only, or wearable and handheld
-    public void onWearableOnlyActionsClick(View view) {
-        PendingIntent handheldActionFeedbackPendingIntent =
-                getActionFeedbackPendingIntent("You invoked the handheld only action", 0);
-
-        PendingIntent wearableActionFeedbackPendingIntent =
-                getActionFeedbackPendingIntent("You invoked the wearable only action", 1);
-
-        PendingIntent bothActionFeedbackPendingIntent =
-                getActionFeedbackPendingIntent("You invoked the action that appears on both devices", 2);
-
-        NotificationCompat.Action handheldOnlyAction = new NotificationCompat.Action(
-                android.R.drawable.ic_media_previous, "Handheld",
-                handheldActionFeedbackPendingIntent);
-
-        NotificationCompat.Action wearableOnlyAction = new NotificationCompat.Action(
-                android.R.drawable.ic_media_next, "Wearable",
-                wearableActionFeedbackPendingIntent);
-
-        NotificationCompat.Action bothAction = new NotificationCompat.Action(
-                android.R.drawable.ic_media_pause, "Both",
-                bothActionFeedbackPendingIntent);
-
-
-        NotificationCompat.WearableExtender wearableExtender = new NotificationCompat.
-                WearableExtender()
-                .addAction(wearableOnlyAction)
-                .addAction(bothAction)
-                .addAction(handheldOnlyAction);
-
-        PendingIntent mainActivityPendingIntent = getMainActivityPendingIntent();
-
-        Notification notification = new NotificationCompat.Builder(this)
-                .setContentTitle("Title")
-                .setContentText("Text")
-                .setSmallIcon(android.R.drawable.ic_media_previous)
-                .setContentIntent(mainActivityPendingIntent)
-                .addAction(bothAction)
-                .extend(wearableExtender)
-                .addAction(handheldOnlyAction)
-                .build();
-
-
-        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
-        notificationManager.notify(NOTIFICATION_ID, notification);
-
-    }
-
-    //Scale image to the size required by notification
-    private Bitmap getScaledLargeIconFromResource(int resource) {
-        Resources res = getResources();
-        int height = (int) res.getDimension(android.R.dimen.notification_large_icon_height);
-        int width = (int) res.getDimension(android.R.dimen.notification_large_icon_width);
-        Bitmap largeIcon = BitmapFactory.decodeResource(res, resource);
-        return Bitmap.createScaledBitmap(largeIcon, width, height, false);
-    }
 
     //Pending activity passes the context of the app. On wearable,
     // it adds "open Application" action button
     @TargetApi(20)
     public void onVoiceReplyClick(View view) {
-        // Intent replyIntent = new Intent(this, activity_chat_detail.class);
-        String[] choices = GenerateNumbers.getNumbers();
-        RemoteInput remoteInput = new RemoteInput.Builder(ChatDetailActivity.EXTRA_VOICE_REPLY)
+
+       // String replyLabel = getResources().getString(R.string.reply_label);
+
+        RemoteInput remoteInput = new RemoteInput.Builder(EXTRA_VOICE_REPLY)
                 .setLabel("Reply")
-                .setChoices(choices)
-                .setChoices(choices)
-                //Set false if voice input option should be excluded
-                .setAllowFreeFormInput(true)
                 .build();
 
-        PendingIntent confirmActionPendingIntent =
-                getActionFeedbackPendingIntent("OK (entry updated)", 0);
 
-        PendingIntent replyPendingIntent = getConversationPendingIntent("Preppy Rabbit", 1);
 
-        NotificationCompat.Action confirmAction = new NotificationCompat.Action(
-                android.R.drawable.ic_media_previous, "OK",
-                confirmActionPendingIntent);
 
-        NotificationCompat.Action replyAction =
-                new NotificationCompat.Action.Builder(R.drawable.ic_action_favorite, ItemID, replyPendingIntent)
-                        .addRemoteInput(remoteInput)
-                        .build();
-
-        NotificationCompat.WearableExtender wearableExtender = new NotificationCompat.WearableExtender()
-                .addAction(confirmAction)
-                .addAction(replyAction);
-
-        Bitmap prettyAvatar = getScaledLargeIconFromResource(R.drawable.apple55);
-
-        Notification notification = new NotificationCompat.Builder(this)
-                .setContentTitle(ItemID)
-                .setContentText(ItemName)
-                .setSmallIcon(R.drawable.ic_action_good)
-                .setContentIntent(getConversationPendingIntent("Pretty Rabbit", 20))
-                .setPriority(Notification.PRIORITY_HIGH)
-                .setDefaults(Notification.DEFAULT_ALL)
-                .setLargeIcon(prettyAvatar)
-                .extend(wearableExtender)
-                .build();
-
-        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
-        notificationManager.notify(NOTIFICATION_ID, notification);
 
     }
-
-    //Pass data from input field to SqlStatements to prepare statements
-//    public void onSendButtonClick(View view) {
-//        Intent intent = new Intent(this, SqlStatements.class);
-//        EditText editText = (EditText) findViewById(R.id.number_of_packages);
-//        String numberOfPackages = editText.getText().toString();
-//        Toast.makeText(this, numberOfPackages + " Items updated", Toast.LENGTH_LONG).show();
+        // Intent replyIntent = new Intent(this, activity_chat_detail.class);
+//        String[] choices = GenerateNumbers.getNumbers();
+//        RemoteInput remoteInput = new RemoteInput.Builder(ChatDetailActivity.EXTRA_VOICE_REPLY)
+//                .setLabel("Reply")
+//                .setChoices(choices)
+//                .setChoices(choices)
+//                //Set false if voice input option should be excluded
+//                .setAllowFreeFormInput(true)
+//                .build();
 //
-//        List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(1);
-//        nameValuePairs.add(new BasicNameValuePair("item Id", ItemID));
-//        nameValuePairs.add(new BasicNameValuePair("item name", ItemName));
-//        nameValuePairs.add(new BasicNameValuePair("amount ", numberOfPackages));
-//        //NOT COMPLETELY IMPLEMENTED
+//        PendingIntent confirmActionPendingIntent =
+//                getActionFeedbackPendingIntent("OK (entry updated)", 0);
 //
-//        //DB connection
-//        try{
+//        PendingIntent replyPendingIntent = getConversationPendingIntent("Preppy Rabbit", 1);
 //
-//            HttpClient httpClient = new DefaultHttpClient() {
+//        NotificationCompat.Action confirmAction = new NotificationCompat.Action(
+//                android.R.drawable.ic_media_previous, "OK",
+//                confirmActionPendingIntent);
 //
-//            HttpPost httpPost = new HttpPost("http://")
+//        NotificationCompat.Action replyAction =
+//                new NotificationCompat.Action.Builder(R.drawable.ic_action_favorite, ItemID, replyPendingIntent)
+//                        .addRemoteInput(remoteInput)
+//                        .build();
 //
-//            };
+//        NotificationCompat.WearableExtender wearableExtender = new NotificationCompat.WearableExtender()
+//                .addAction(confirmAction)
+//                .addAction(replyAction);
 //
-//        }catch (Exception e){
+//        Bitmap prettyAvatar = getScaledLargeIconFromResource(R.drawable.apple55);
 //
-//        }
+//        Notification notification = new NotificationCompat.Builder(this)
+//                .setContentTitle(ItemID)
+//                .setContentText(ItemName)
+//                .setSmallIcon(R.drawable.ic_action_good)
+//                .setContentIntent(getConversationPendingIntent("Pretty Rabbit", 20))
+//                .setPriority(Notification.PRIORITY_HIGH)
+//                .setDefaults(Notification.DEFAULT_ALL)
+//                .setLargeIcon(prettyAvatar)
+//                .extend(wearableExtender)
+//                .build();
+//
+//        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
+//        notificationManager.notify(NOTIFICATION_ID, notification);
+//
 //    }
 
     @Override
